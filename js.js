37c337552d2dab9508b9648cbeed877a3bf3f4b8
Prashantkaushik07 <160236438+Prashantkaushik07@users.noreply.github.com>
2024-02-19 19:23:55 +0530

    first Commit

---
+++ js.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Javascript</title>
+<script>
+ let name=10;
+ let c=20;
+ let d=50;
+
+</script>
+<body>
+</body>
+</head>
+</html>
---
+++ test.js
@@ -0,0 +1,312 @@
+// /*let marks=[20,40,30,39,56];
+
+
+// //for (let i=0; i<marks.length; i++){
+//  //console.log(marks[i]);
+// */
+//var marks=[20,40,30,39,67];
+//marks[3]=32;
+//console.log(marks);
+
+//let marks=[40,20,30,45,189,100,2999,367,478,45678,98765,357890,23456789];
+//console.log(marks);
+
+//const marks=[30,40,67,87,98,89,79,67,75,84,02,123];
+//marks[7]=89;
+//console.log(marks);
+
+
+
+
+
+
+//let cities=["delhi","Noida","pune","banglore","chennai","hyderabad","mumbai","kolkata","surat","ahmedabad","jaipur"];
+  //for (let city of cities=cities){
+    //     console.log(city.toUpperCase());
+ //}
+
+ //const student={
+   // fullName:"Prashant Kaushik",
+     //    age:24,
+       // cgpa:7.2,
+        //city:"Noida",
+//}
+
+ //student["cgpa"]="8.2";
+ //console.log(student);
+
+
+// /*let student=[200,17,26,78,39,29,90,289,367,876,368];
+
+// student[5]=30;
+
+
+// console.log(student);*/
+
+// // let a=25;
+// let b=35;
+// console.log("a = ", a " & b = ",b );
+// console.log("a + b = ", a + b );
+// console.log("a - b = ", a - b );
+// console.log("a * b = ", a * b );
+// console.log("a / b = ", a / b );
+// console.log("a ** b = ", a ** b );
+
+
+// unary Operators
+
+// let a=5;
+// let b=25;
+// console.log(" a = ", a ," & b = " , b );
+// console.log(++a );
+
+//Assignment Operators
+//let a=25;
+//let b=35;
+
+//console.log("a + = ", a ," & b = " ,b );
+//console.log("++a= ", ++a);
+
+ // let a=65;
+// let b=87;
+
+// console.log("a + = ", a ," & b + = ", b);
+// a=a + 1;
+// console.log(a);
+
+
+//Comparison Operators
+
+// let a=50;
+// let b=65;
+
+
+// console.log("50 == 65", a==b);
+
+// let cities=["noida","delhi","mumbai","banglore","hyderabad","chennai","kolkata"];
+// cities.unshift("pune");
+// cities.pop("kolkata");
+// cities.shift("hyderabad");
+// console.log(cities);
+
+//Conditional Statement
+
+// let age=25;
+
+// if (age >= 18){
+//   console.log("you can vote");
+// }
+
+
+// for (let num = 1; num <= 10000000; num++);{
+//   console.log("Prashant kaushik");
+// }
+// console.log("lopp has ended");
+
+
+// for (var i = 1; i <= 5; i++);{
+//   console.log("i =  ",  i );
+// }
+// console.log(i);
+
+ //let i=10;
+ //do {
+   //console.log(" i = ", i);
+   //i++;
+ //} while(i <= 100 );
+
+//For in loop
+
+// let student = {
+//   name:"Prashant Kaushik",
+//   age:24,
+//   cgpa:7.2,
+//   ispass:true,
+// }
+//console.log("key= ", key, "value= ", arr[key]);
+
+// let arr=[1,2,3,4,5];
+// for (let key in arr)
+// {
+//   console.log("key= ", key, "value= ", arr[key]);
+// }
+
+//Array Method 14-02-24
+//Concat() method
+// const student=["Prashant","Rahul","Rohan","kapil","Akash",];
+// const stu1=["yogesh","vishvesh","akshay","vivek"];
+// const result=student.concat(stu1);
+// console.log(result);
+
+
+//copywithin()
+
+// let student=["Prashant"];
+// let result= [];
+
+// // console.log(student);
+//  for (let i=0; i<student.length; i++){
+//   let final = student.lenght[i];
+//   for(let j=0; final.length i++);
+//   console.log(final);
+//  }
+// }   student console.log(student.toString());
+// console.log(student.from);
+// student.pop();
+// student.push("delhi");
+// student
+
+// console.log(student);
+
+
+//.Tostring()
+
+// let cities=["Noida","Delhi","Mumbai","Gurgaon","Kolkata"];
+// console.log(cities);
+// console.log(cities.toString());
+// console.log(cities);
+
+
+//error code
+
+// const marks=[20,30,40,50,60,70];
+// console.log(marks);
+// marks.splice(2,1,100);
+// console.log(marks); 
+
+// let companies=["Bloomberg","IBM","Microsoft","Netflix","Uber","Google"];
+// console.log(companies);
+// companies.shift()
+// console.log(companies);
+// companies.splice(3,1,"Ola")
+// console.log(companies)
+// companies.push("Amazon");
+// console.log(companies)
+
+
+// const cities=["Noida","Delhi","Mumbai","kolkata"];
+// console.log(cities);
+// cities.shift();
+// console.log(cities);
+
+// const num=[1,2,3,4,5,6];
+// console.log(num[3]);
+
+// const marks=[20,30,40,50,60,80];
+// console.log(marks);
+// marks.splice(3,1,100);
+// console.log(marks);
+
+// const student=["Prashant","Vivek","Rahul","Akash","Ayush","Hemant"];
+// console.log(student);
+// const result=student.copyWithin("Ayush");
+// //student.entries(student);
+// console.log(result);
+
+// const student=["Prashant","Akash","Rahul","Ayush"];
+// const result=student.toCharArray();
+// console.log(result);
+
+// let marks=[10,200,300,40,50,60]
+// let marks1=[900,600,450,350,260]
+// let marks2=[200,400,500,200,700,800]
+// console.log(Array.of(marks,marks1,marks2));
+
+// const student=["Prashant","Vivek","Rahul","Akshay","Ayush"]
+// const stu1=["Kapil","Rohan","Harsh","Gaurav","Ajay"];
+
+
+// const result=student.from(stu1);
+// console.log(result);
+  
+//   for (let i = 0;  i < result.length; i++) {
+//   const result1 = result[i];
+  
+// }
+// console.log(result);
+
+
+// console.log(result.join());
+// console.log(result.from("Prashant"));
+// console.log(result);
+// student.indexOf("");
+// console.log(result);
+// student.toString("Student");
+// console.log(result);
+// student.unshift("Mr bean");
+// console.log(result);
+// conso
+
+// const student=["Prashant","Vivek","Rahul","Akshay","Ayush"]
+// const stu1=["Kapil","vishvesh","Hemant","Anuj"]
+// const result=student.concat(stu1);
+// console.log(result);
+
+// const student=["Prashant","Vivek","Rahul","Akshay","Ayush"]
+// console.log(student.includes("Vivek"));
+
+// const days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+// const result=days.keys();
+// console.log(result)
+
+// for (const key of result){
+//  console.log(key);
+// }
+  
+// const student=["Prashant","Vivek","Rahul","Akshay","Ayush"];
+// console.log(student.copyWithin(2,1));
+// console.log(student);
+
+// const marks=[56,78,94,37,45];
+// const iterater=marks.entries();
+// console.log(iterater.next().value);
+// console.log(iterater.next().value);
+// console.log(iterater.next().value);
+// console.log(iterater.next().value);
+// console.log(iterater.next().value);
+// console.log(iterater.next().value);
+// console.log(marks);
+
+//fill() Method
+// const num=[0,1,2,3,4,5,6,7,8,9]
+// console.log(num.fill(2,4));
+
+// const student=["Nitin","Manish","Vishal","Shubham","Harry","john"];
+// // console.log(student.findIndex("Nitin"));
+// console.log(student);
+// console.log(student.indexOf());
+
+
+// const student=["Vivek","Manish","Akshay","john","Rahul","harry","nitin"]
+// console.log(student.lastIndexOf("Rahul"))
+
+// const arr=[12,1815,1987,19998,28451,12786]
+// arr.sort();
+// console.log(arr)
+
+// const arr=[1000,750,350,260,745,100,500]
+// arr.sort()
+// console.log(arr);
+
+// const arr=[1,15,[5,20],32,115,2,22]
+// arr.sort((a,b)=>a-b);
+// console.log
+// console.log(arr);
+
+// const  a =[];
+// console.log(typeof a);
+
+//  print all element array using for loop
+// let student=["PrashantKaushik","Akash","Ayush","Hemant","Kapil","Saurabh","Ankit",]
+
+// for (let i = 0; i < student.length; i++) {
+//   console.log(Array.from(student[i].toUpperCase()));
+// }
+
+var a = 40;
+var b = 20;
+if(a > b){
+  alert("A is greater than B");
+}else{
+alert( b is a greater );
+}
